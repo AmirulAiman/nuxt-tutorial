e936dda289eebf0c36c53870d71b365a26fc1dee
AmirulAiman <aiman_amirul@ymail.com>
2018-05-03 16:58:18 +0800

    test4

--- pages/index.vue
+++ pages/index.vue
@@ -15,9 +15,45 @@ export default {
   components: {
     'post-list' : PostList
   },
-  data() {
-    return {
-      loadedPosts : [
+  asyncData(context, callback){
+    setTimeout(() => {
+      callback( null, {
+        loadedPosts: [
+          {
+            id: '1',
+            title: 'First Post',
+            previewText: 'This is my 1st post',
+            thumbnailLink: 'https://i.ytimg.com/vi/NamzxgmpM7s/maxresdefault.jpg'
+          },
+          {
+            id: '2',
+            title: 'Second Post',
+            previewText: 'This is my 2nd post',
+            thumbnailLink: 'https://78.media.tumblr.com/ddc5fd4673723188622d6c238f6b4ac0/tumblr_outtddRBI71v9x50oo1_500.gif'
+          },
+          {
+            id: '3',
+            title: 'Third Post',
+            previewText: 'This is my 3rd post',
+            thumbnailLink: 'https://cdna.artstation.com/p/assets/images/images/001/705/970/large/quy-n-anh-yellow-house-03.jpg?1451367032'
+          },
+          {
+            id: '4',
+            title: 'Forth Post',
+            previewText: 'This is my 4th post',
+            thumbnailLink: 'https://i.ytimg.com/vi/AD834QHtbfU/maxresdefault.jpg'
+          }]
+      })
+    })
+  },
+  // data() {
+  //   return {
+  //     loadedPosts : []
+  //   }
+  // },
+  created() {
+    setTimeout(() => {
+      this.loadedPosts = [
         {
           id: '1',
           title: 'First Post',
@@ -43,7 +79,7 @@ export default {
           thumbnailLink: 'https://i.ytimg.com/vi/AD834QHtbfU/maxresdefault.jpg'
         }
       ]
-    }
+    }, 1500);
   }
 }
 </script>
